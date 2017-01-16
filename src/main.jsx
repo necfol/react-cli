a7d716c28374c5d262f350c606fb0da37d4512a1
necfol <necfoll@gmail.com>
2017-01-16 17:59:32 +0800

    add react-router demo

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './page/App/App';
+import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
+import Index from './page/Index/Index';
 import main from './main.css';
 ReactDOM.render(
-  <App />,
-  document.body.appendChild(document.createElement('div'))
+    (<Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Index} />
+        </Route>
+    </Router>),
+    document.body.appendChild(document.createElement('div'))
 );
--- src/page/App/App.jsx
+++ src/page/App/App.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 require('./App.css');
 
-export default () => <h1>こんにちは世界</h1>;
 
-/*
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+    };
   }
-
   render() {
     return (
-      <h1>Hello World</h1>
+        <div>
+            <h1>こんにちは世界</h1>
+            <div>
+                {this.props.children}
+            </div>
+        </div>
     );
   }
 }
-*/
+
